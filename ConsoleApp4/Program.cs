73b277e4526ef164215c73e59fb47e9a1e02b001
USER <USER@DESKTOP-TRQG01D>
2022-12-09 13:05:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Extensions
+{
+    public static IEnumerable<string> Split(this string str, int n)
+    {
+        if (String.IsNullOrEmpty(str) || n < 1)
+        {
+            throw new ArgumentException();
+        }
+
+        return Enumerable.Range(0, str.Length / n)
+                        .Select(i => str.Substring(i * n, n));
+    }
+}
+
+public class Example
+{
+    public static void Main()
+    {
+        string str = "123456789394348549560954675";
+        int size = 5;
+        Console.WriteLine(str);
+
+        IEnumerable<string> s = str.Split(size);
+        Console.WriteLine(String.Join(Environment.NewLine, s));
+    }
+}
+
+
